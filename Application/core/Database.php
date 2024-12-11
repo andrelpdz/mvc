1e6d2d6a5ee8c702b455dc3b5fcbee014c67da8c
AndreLPDZ <lpdzandre@gmail.com>
2024-12-11 13:56:52 -0300

    Update Database.php
    Alterado de PDO para mySqli Query

--- Application/core/Database.php
+++ Application/core/Database.php
@@ -1,66 +1,31 @@
 <?php
-
 namespace Application\core;
 
-use PDO;
-class Database extends PDO
-{
-  // configuração do banco de dados
-  private $DB_NAME = 'mvc_db';
-  private $DB_USER = 'postgres';
-  private $DB_PASSWORD = 'postgres';
+class Database{
+
+  private $DB_NAME = 'andrelpdz';
+  private $DB_USER = 'andrelpdz';
+  private $DB_PASSWORD = 'AlpdzPass';
   private $DB_HOST = 'localhost';
   private $DB_PORT = 5432;
 
-  // armazena a conexão
   private $conn;
 
-  public function __construct()
-  {
-    // Quando essa classe é instanciada, é atribuido a variável $conn a conexão com o db
-    $this->conn = new PDO("pgsql:dbname=$this->DB_NAME;host=$this->DB_HOST;port=$this->DB_PORT;user=$this->DB_USER;password=$this->DB_PASSWORD");
+  public function __construct(){
+    $this->conn = mysqli_connect($this->DB_HOST,$this->DB_USER,$this->DB_PASSWORD) or die("Erro: Conexão");
+    
+    # Selecionar banco
+    mysqli_select_db($this->conn, $this->DB_NAME) or die("Erro: Banco");
+    
+    # Definir charset UTF-8
+    mysqli_query($this->conn,"SET NAMES 'utf8'");
+    mysqli_query($this->conn,"SET character_set_connection=utf8");
+    mysqli_query($this->conn,"SET character_set_client=utf8");
+    mysqli_query($this->conn,"SET character_set_results=utf8");  
   }
 
-  /**
-  * Este método recebe um objeto com a query 'preparada' e atribui as chaves da query
-  * seus respectivos valores.
-  * @param  PDOStatement  $stmt   Contém a query ja 'preparada'.
-  * @param  string        $key    É a mesma chave informada na query.
-  * @param  string        $value  Valor de uma determinada chave.
-  */
-  private function setParameters($stmt, $key, $value)
-  {
-    $stmt->bindParam($key, $value);
+  public function executeQuery(string $query){
+    $q = mysqli_query($this->conn,$query);
+    return $q;
   }
-
-  /**
-  * A responsabilidade deste método é apenas percorrer o array de com os parâmetros
-  * obtendo as chaves e os valores para fornecer tais dados para setParameters().
-  * @param  PDOStatement  $stmt         Contém a query ja 'preparada'.
-  * @param  array         $parameters   Array associativo contendo chave e valores para fornece a query
-  */
-  private function mountQuery($stmt, $parameters)
-  {
-    foreach( $parameters as $key => $value ) {
-      $this->setParameters($stmt, $key, $value);
-    }
-  }
-
-  /**
-  * Este método é responsável por receber a query e os parametros, preparar a query
-  * para receber os valores dos parametros informados, chamar o método mountQuery,
-  * executar a query e retornar para os métodos tratarem o resultado.
-  * @param  string   $query       Instrução SQL que será executada no banco de dados.
-  * @param  array    $parameters  Array associativo contendo as chaves informada na query e seus respectivos valores
-  *
-  * @return PDOStatement
-  */
-  public function executeQuery(string $query, array $parameters = [])
-  {
-    $stmt = $this->conn->prepare($query);
-    $this->mountQuery($stmt, $parameters);
-    $stmt->execute();
-    return $stmt;
-  }
-
 }
